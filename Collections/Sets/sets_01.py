67c8f4f3dea0fb64c2524d84ebeada7a556293ee
Kris Thomas <kresthomas1914@gmail.com>
2021-11-12 18:30:49 -0600

    ah

--- Collections/Sets/sets_01.py
+++ Collections/Sets/sets_01.py
@@ -7,6 +7,10 @@ print(len(thisset))
 
 # set can only holds strings, integers and booleans
 
+# turn something into a set by using set()
 state = 'Mississippi'
 state_set = set(state)
 print(state_set)
+
+# add to a set
+thisset.add('orange')
