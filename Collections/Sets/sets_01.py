e27fdc40c4f7ee8761c90de3d332edd171e974d9
Kris Thomas <kresthomas1914@gmail.com>
2021-11-12 18:28:50 -0600

    added set drills

---
+++ Collections/Sets/sets_01.py
@@ -0,0 +1,12 @@
+#unorderd collections of unique elements
+thisset = {"apple", "banana", "cherry", "apple"}
+print(thisset)
+
+# len of a set
+print(len(thisset))
+
+# set can only holds strings, integers and booleans
+
+state = 'Mississippi'
+state_set = set(state)
+print(state_set)
---
+++ Collections/Tuples/tuples_01.py
@@ -0,0 +1,18 @@
+# similar to lists, but they're immutable
+# once an element is inside a tuple, it cannot be reassigned
+# tuples use parenthesis
+
+
+t = (1,2,3)
+my_list = [1, 2, 3]
+# tuples only have two methods
+t.index(2)
+t.count()
+
+len(t) # 3
+print(t[0]) # can access an index
+print(t[1:]) # interesting, this returns a tuple
+t.index(1) # returns the index of the first occurance of an element
+
+#tuples can ensure data integrity
+
